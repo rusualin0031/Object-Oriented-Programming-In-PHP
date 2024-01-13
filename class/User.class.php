d4edbfe55249e8f07d9689c75ad48d478bea66a2
rusualin0031 <123866644+rusualin0031@users.noreply.github.com>
2024-01-13 11:10:44 +0100

    Create Class

---
+++ class/User.class.php
@@ -0,0 +1,36 @@
+<?php
+class User
+{
+    public $first_name;
+    public $last_name;
+    public $username;
+    public $passord;
+    public $status;
+
+    public function __construct($first_name, $last_name, $status)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->status = $status;
+    }
+
+    public function full_name()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    public function status()
+    {
+        return $this->status;
+    }
+
+    public function __destruct(){
+        echo "Object is destucted! <br>";
+    }
+}
+
+$user = new User("Peter", "Parker", "1");
+echo $user->full_name();
+echo "<br>";
+echo $user1->status();
+echo "<br><br>";
